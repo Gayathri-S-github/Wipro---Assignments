1638ae7029cbc252a848d11cb2dd58a39f811f3f
Gayathri-S-github <57089967+Gayathri-S-github@users.noreply.github.com>
2020-06-16 11:16:47 +0530

    Create Assignment8-Array.java

---
+++ Assignment8-Array.java
@@ -0,0 +1,79 @@
+
+// Highest frequency Element in an Array.
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+public class HighestFrequencyOfArray{
+public static void main(String args[]) {
+   
+Scanner scan =new Scanner(System.in);
+int n=scan.nextInt();
+int inputArray[]=new int[n];
+for(int i=0;i<n;i++)
+inputArray[i]=scan.nextInt();
+
+   //Creating HashMap object with elements as keys and their occurrences as values
+         
+        HashMap<Integer, Integer> elementCountMap = new HashMap<Integer, Integer>();
+         
+        //Inserting all the elements of inputArray into elementCountMap
+         
+        for (int i : inputArray) 
+        {
+            if (elementCountMap.containsKey(i))
+            {
+                //If an element is present, incrementing its count by 1
+                 
+                elementCountMap.put(i, elementCountMap.get(i)+1);
+            }
+            else
+            {
+                //If an element is not present, put that element with 1 as its value
+                 
+                elementCountMap.put(i, 1);
+            }
+        }
+         
+        int element = 0;
+         
+        int frequency = 1;
+         
+        //Iterating through elementCountMap to get the most frequent element and its frequency
+         
+        Set<Entry<Integer, Integer>> entrySet = elementCountMap.entrySet();
+         
+        for (Entry<Integer, Integer> entry : entrySet) 
+        {
+            if(entry.getValue() > frequency)
+            {
+                element = entry.getKey();
+                 
+                frequency = entry.getValue();
+            }
+        }
+         
+        //Printing the most frequent element in array and its frequency
+         
+        if(frequency > 1)
+        {
+            System.out.println("Input Array : "+Arrays.toString(inputArray));
+             
+            System.out.println("The most frequent element : "+element);
+             
+            System.out.println("Its frequency : "+frequency);
+             
+            System.out.println("========================");
+        }
+        else
+        {
+            System.out.println("Input Array : "+Arrays.toString(inputArray));
+             
+            System.out.println("No frequent element. All elements are unique.");
+             
+            System.out.println("=========================");
+	}
+}
+
+}
